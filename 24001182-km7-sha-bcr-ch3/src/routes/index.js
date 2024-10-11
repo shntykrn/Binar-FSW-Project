660f116720bef52604b3c72ceed4f1fbe766e50d
shntykrn <shntykrn@gmail.com>
2024-10-11 23:13:07 +0700

    updated

--- 24001182-km7-sha-bcr-ch3/src/routes/index.js
+++ 24001182-km7-sha-bcr-ch3/src/routes/index.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.json({ message: "ping successfully" });
 });
-router.use("/cars", carRouter);
+router.use("/cars", carsRouter);
 
 module.exports = router;
